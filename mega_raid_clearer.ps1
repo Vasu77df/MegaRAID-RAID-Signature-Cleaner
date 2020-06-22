fd6506d0f78858de031b37474fe3bfc56808aa7a
Vasu77df <vasu3797@gmail.com>
2020-06-22 14:33:54 -0500

    powershell script

---
+++ mega_raid_clearer.ps1
@@ -0,0 +1,5 @@
+﻿Set-Location /root/8-07-14_MegaCLI/Windows
+
+Write-Host "---------------------Welcome to RAID Cleaner-------------------"
+Write-Host "List of all the available drives attached to the controller: "
+./MegaCli
