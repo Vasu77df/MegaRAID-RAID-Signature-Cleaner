cf0c433946b64554353120ca829f3c5ae36dfe7f
Vasu77df <vasu3797@gmail.com>
2020-06-25 01:04:10 -0500

    Final Deployment code

--- practice.ps1
+++ practice.ps1
@@ -1,22 +1,23 @@
-﻿
+
+
 function Start-Clearing {
     Write-Host "Clearing RAID configuration of Disk in Slot: $slot_number"
     Start-Sleep -Seconds 1
     Write-Host "Clearing Foreign Configuration......"
-    .\MegaCli.exe -CfgForeign -Clear -aALL
+    
     Start-Sleep -Seconds 1
     Write-Host "Making the Disk into Unconfigured Good if in Unconfigured Bad state (ignore failure if occurs)"
-    .\MegaCli.exe -PDMakeGood -PhysDrv[64:$slot_number] -aALL
+    
     Start-Sleep -Seconds 1
     Write-Host "Converting the Disk into JBOD mode....."
-    .\MegaCli.exe -PDMakeJBOD -PhysDrv[64:$slot_number] -aALL
+    
     Start-Sleep -Seconds 1
     Write-Host "----------------Done Clearing RAID Configuration---------------"
     Start-Sleep -Seconds 1
 }
 
 function Get-Confirmation {
-    $OPT = Read-Host -Prompt "Are you sure you want to clear RAID configuration in slot: $slot_number ?[y/n] "
+    $OPT = Read-Host -Prompt "Are you sure you want to clear RAID configuration in slot: $slot_number ?[y/n]: "
     
     switch ($OPT) {
         'Y' { Start-Clearing }
@@ -38,18 +39,17 @@ function Get-Confirmation {
 $RESTART = $true
 Do {
 Write-Host "---------------------Welcome to RAID Cleaner-------------------"
-Set-Location C:\Users\Administrator\Desktop\8-07-14_MegaCLI\Windows
 Write-Host "List of all the available drives attached to the controller: "
-.\MegaCli.exe -PDList -aALL | Select-String -Pattern "Enclosure Device Id", "Slot Number", "Drive's Position", "Device Id", "Firmware State", "Foreign State"
 
-$nu_disk = Read-Host -Prompt "How many disks would you like to clear? "
+
+$nu_disk = Read-Host -Prompt "How many disks would you like to clear?: "
 
 if ( $nu_disk -notmatch "^[0-9]+$" ) {
-    $nu_disk = Read-Host -Prompt "Invalid Input! Please enter the number of disks to be cleared "
+    $nu_disk = Read-Host -Prompt "Invalid Input! Please enter the number of disks to be cleared: "
 }
 
 for ($i = 0; $i -lt $nu_disk; $i++) {
-    $slot_number = Read-Host -Prompt "You can clean the Foreign RAID configuration of the Disk by specifing the slot number(please enter the slot of disk one by one) "
+    $slot_number = Read-Host -Prompt "You can clean the Foreign RAID configuration of the Disk by specifing the slot number(please enter the slot of disk one by one): "
     if ($slot_number -notmatch "^[0-9]+$") {
         $slot_number = Read-Host -Prompt "Invalid Input! Please enter the slot number: "
     }
@@ -60,15 +60,12 @@ for ($i = 0; $i -lt $nu_disk; $i++) {
 
     Get-Confirmation
 
-    if ( $i -lt $nu_disk - 1) {
+    if ( $i -lt $nu_disk) {
         Write-Host "---------------------------Next Disk---------------------------"
     }
 }
 
 Write-Host "--------------------- Current Configuration--------------------"
-.\MegaCli.exe -PDList -aALL | Select-String -Pattern "Enclosure Device Id", "Slot Number", "Drive's Position", "Device Id", "Firmware State", "Foreign State"
-
-Set-Location C:\Users\Administrator
 
 
 Start-Sleep -Seconds 2
@@ -76,8 +73,9 @@ $EXIT_CONFIRMATION = Read-Host -Prompt "Would you like to exit the program?[y/n]
 if ($EXIT_CONFIRMATION -match 'y') {
     Write-Host "----------------------------Exiting----------------------------"
     $RESTART=$false
-    Start-Sleep -Seconds 5
 }else {
     $RESTART=$true
 }
-} While ($RESTART) 
+
+}
+While ($RESTART) 
